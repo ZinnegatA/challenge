c810f1d0c71af0431cda3b0c0413cc790435cad9
Zinnegata <ruslanibr123@gmail.com>
2023-09-25 19:00:15 +0600

    Added ESLint, Prettier

--- server/orm.config.ts
+++ server/orm.config.ts
@@ -1,17 +1,19 @@
-import "reflect-metadata"
-import { DataSource } from "typeorm"
-import { User } from "./src/entities/User"
+import "reflect-metadata";
+import { DataSource } from "typeorm";
+import { User } from "./src/entities/User";
+import { Task } from "./src/entities/Task";
+import { Run } from "./src/entities/Run";
 
 export const AppDataSource = new DataSource({
-    type: "postgres",
-    host: "localhost",
-    port: 5432,
-    username: "test",
-    password: "test",
-    database: "testdb",
-    synchronize: true,
-    logging: false,
-    entities: [User],
-    migrations: [],
-    subscribers: [],
-})
+  type: "postgres",
+  host: "localhost",
+  port: 5432,
+  username: "postgres",
+  password: "postgres",
+  database: "challenge",
+  synchronize: true,
+  logging: false,
+  entities: [User, Task, Run],
+  migrations: [`${__dirname}/migrations/**/*{.js,.ts}`],
+  subscribers: [`${__dirname}/subscribers/**/*{.js,.ts}`],
+});
