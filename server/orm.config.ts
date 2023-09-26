17359188ac5ecba5fd4db8288c7b68e655209cdd
Zinnegata <ruslanibr123@gmail.com>
2023-09-26 15:55:34 +0600

    Configured ESLint and Prettier, fixed migrations issue

--- server/orm.config.ts
+++ server/orm.config.ts
@@ -1,19 +1,20 @@
-import "reflect-metadata";
-import { DataSource } from "typeorm";
-import { User } from "./src/entities/User";
-import { Task } from "./src/entities/Task";
-import { Run } from "./src/entities/Run";
+import 'reflect-metadata';
+import path from 'path';
+import { DataSource } from 'typeorm';
+import { User } from './src/entities/User';
+import { Task } from './src/entities/Task';
+import { Run } from './src/entities/Run';
 
 export const AppDataSource = new DataSource({
-  type: "postgres",
-  host: "localhost",
+  type: 'postgres',
+  host: 'localhost',
   port: 5432,
-  username: "postgres",
-  password: "postgres",
-  database: "challenge",
+  username: 'postgres',
+  password: 'postgres',
+  database: 'challenge',
   synchronize: true,
   logging: false,
   entities: [User, Task, Run],
-  migrations: [`${__dirname}/migrations/**/*{.js,.ts}`],
-  subscribers: [`${__dirname}/subscribers/**/*{.js,.ts}`],
+  migrations: [path.join(__dirname, '/src/migrations/**/*{.js,.ts}')],
+  subscribers: [path.join(__dirname, '/src/subscribers/**/*{.js,.ts}')],
 });
