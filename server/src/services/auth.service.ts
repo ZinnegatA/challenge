f56d2a05b5de2d5606cc855757aaf865d91e1bbc
ilias-bikenov <ilias.bikenov@gmail.com>
2023-10-05 11:57:41 +0600

    refactor auth

--- server/src/services/auth.service.ts
+++ server/src/services/auth.service.ts
@@ -1,21 +1,15 @@
 import type { Request, Response } from 'express';
-import { validationResult } from 'express-validator';
 import { Admin } from '../entities/Admin';
 import { AppDataSource } from '../../orm.config';
 import { generateAccessToken } from '../utils/auth.helper';
 import { compareSync } from 'bcrypt';
 import 'dotenv/config';
+import { validateRequest } from '../utils/validation.helper';
 
 export class AuthService {
   async adminLogin(req: Request, res: Response): Promise<Response> {
     try {
-      const errors = validationResult(req);
-
-      if (!errors.isEmpty()) {
-        const errorMessage = errors.array()[0].msg;
-
-        return res.status(401).json({ message: errorMessage });
-      }
+      validateRequest(req, res);
 
       const { username, password } = req.body;
 
