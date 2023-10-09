d54f52b2bbe1cc43edff09974bfbe4300e35f0f1
ilias_bikenov <ilias.bikenov@gmail.com>
2023-10-09 14:42:17 +0600

    Dockerize app

--- server/orm.config.ts
+++ server/orm.config.ts
@@ -8,7 +8,7 @@ import { Admin } from './src/entities/Admin';
 
 export const AppDataSource = new DataSource({
   type: 'postgres',
-  host: 'localhost',
+  host: 'postgres',
   port: 5432,
   username: 'postgres',
   password: 'postgres',
