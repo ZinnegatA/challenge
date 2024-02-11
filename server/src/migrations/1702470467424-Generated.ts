b20d54376aa07d394b5fde47073c2f9b327b5d23
Azat Askerov <azat.askerov@tvh.com>
2024-02-12 04:27:10 +0600

    add missing migration

---
+++ server/src/migrations/1702470467424-Generated.ts
@@ -0,0 +1,22 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class Generated1702470467424 implements MigrationInterface {
+    name = 'Generated1702470467424';
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(
+            `CREATE TABLE "participation" ("id" SERIAL NOT NULL, "score" integer NOT NULL, "runId" integer, "userId" integer, CONSTRAINT "PK_ba5442bab90fc96ddde456c69e1" PRIMARY KEY ("id"))`,
+        );
+        await queryRunner.query(
+            `ALTER TABLE "participation" ADD CONSTRAINT "FK_10a33a8efa8f45a57ced59319f3" FOREIGN KEY ("runId") REFERENCES "run"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`
+        );
+        await queryRunner.query(
+            `ALTER TABLE "participation" ADD CONSTRAINT "FK_8ed09e9b7e0a3a150f9515f254f" FOREIGN KEY ("userId") REFERENCES "user"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`
+        );
+        await queryRunner.query(
+            `INSERT INTO "migrations"("timestamp", "name") VALUES ($1, $2) -- PARAMETERS: [1702470467424,"Generated1702470467424"]`
+        );
+    }
+
+    public async down() {}
+}
